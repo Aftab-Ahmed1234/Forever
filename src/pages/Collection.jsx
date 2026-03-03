a88083253eec36fb97509e827d89d6b3913b0750
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-03 17:51:59 +0500

    changes

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -9,7 +9,14 @@ const Collection = () => {
        <div className="min-w-60">
         <p className='my-2 text-xl flex text-center cursor-pointer gap-2'>FILTERS</p>
         {/* Category Filter */}
-        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilters ? '' : "hidden"} sm:block`}></div>
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilters ? '' : "hidden"} sm:block`}>
+          <p className="mb-3 text-sm font-medium ">CATEGORIES</p>
+             <div className="flex flex-col gap-2 font-light text-sm text-gray-700">
+              <p className="flex gap-2">
+                <input type="text" />
+              </p>
+             </div>
+        </div>
        </div>
     </div>
   )
