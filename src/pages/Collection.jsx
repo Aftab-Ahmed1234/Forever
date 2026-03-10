325cc9a5a3eeca6dbd8142e1bdf2d193a730c53e
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-10 18:29:35 +0500

    changes

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -31,8 +31,13 @@ const Collection = () => {
   }, [products])
  const applyFilter =() => {
   let productsCopy = products.slice();
- 
+ if(category.length > 0){
+  productsCopy = productsCopy.filter( item => category.includes(item.category))
+ }
 }
+useEffect(() => { 
+  applyFilter();
+}, [category, subCategory])
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       {/* Filter Options  */}
