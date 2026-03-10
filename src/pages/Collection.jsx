cfb2784c212c328b3e13c0b970b1f5d3ea2157b2
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-10 18:19:29 +0500

    changes

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -31,8 +31,8 @@ const Collection = () => {
   }, [products])
 
   useEffect(() => { 
-    console.log(category)
-  }, [category])
+    console.log(subCategory)
+  }, [subCategory])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
