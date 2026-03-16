b7409c0b8da29ec81503d7faa37563c98b1788a0
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-16 17:16:40 +0500

    changing

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import { useContext } from 'react'
 const SearchBar = () => {
     const {search,setSearch, showSearch,setshowSearch}=useContext(ShopContext);
   return  showSearch ? (
-    <div className='border-t border-b bg-gray-50'>
+    <div className='border-t border-b bg-gray-50 text-center'>
       
     </div>
   ) : null
