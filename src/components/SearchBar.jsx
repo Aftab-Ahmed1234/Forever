dcaec74e81e00d6aab3ea0c20d0f06ccf9adee01
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-16 18:03:59 +0500

    changing

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ const SearchBar = () => {
   return  showSearch ? (
     <div className='border-t border-b bg-gray-50 text-center'>
       <div className='inline-flex item-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4  sm:w-1/2 '>
-        <input type="text"  />        
+        <input type="text"  placeholder=''/>        
       </div>
       
     </div>
