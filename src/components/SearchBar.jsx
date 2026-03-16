64195a6027e72f1358318828f4588175a9ac8130
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-16 18:01:21 +0500

    changing

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ const SearchBar = () => {
     const {search,setSearch, showSearch,setshowSearch}=useContext(ShopContext);
   return  showSearch ? (
     <div className='border-t border-b bg-gray-50 text-center'>
-      <div className='inline-flex item-center justify-center border border-gray-400 px-5 py-2'>
+      <div className='inline-flex item-center justify-center border border-gray-400 px-5 py-2 my-5 '>
         
       </div>
       
