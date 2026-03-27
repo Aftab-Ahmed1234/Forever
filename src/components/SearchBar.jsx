7e2583b07a7e1f20df723ca647fd3135d859bd1d
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-27 22:11:49 +0500

    changing

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { useContext } from 'react'
 import { assets } from '../assets/assets';
 import { ShopContext } from '../context/ShopContext';
+import { useLocation } from 'react-router-dom';
 
 const SearchBar = () => {
-    const {search,setSearch, showSearch,setshowSearch}=useContext(ShopContext);
+  const {search,setSearch, showSearch,setshowSearch}=useContext(ShopContext);
+  const location= useLocation();
+
+
   return  showSearch ? (
     <div className='border-t border-b bg-gray-50 text-center'>
       <div className='inline-flex item-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4  sm:w-1/2 '>
