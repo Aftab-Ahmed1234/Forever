49562c048f20848fcc07c0377c5aa6d2911b049b
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-27 22:33:49 +0500

    changing

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ const SearchBar = () => {
   
   useEffect(()=>{
     console.log(location.pathname)
-     if(location.pathname.includes('Collection').){
+     if(location.pathname.includes('Collection').showSearch){
        location.pathname
      }
   },[location])
