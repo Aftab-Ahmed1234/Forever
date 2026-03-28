cf24198e50a4af0f0f645e080f4f71acdcb8100a
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-28 17:55:50 +0500

    changing

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 
 const SearchBar = () => {
   const {search,setSearch, showSearch,setshowSearch}=useContext(ShopContext);
+  const 
   const location= useLocation();
   
   useEffect(()=>{
