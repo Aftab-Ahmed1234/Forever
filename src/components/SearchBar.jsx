3a3abb58b562d62f26a69937f6fa5c1c9e2bcc44
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-28 17:58:29 +0500

    changing

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,7 +14,7 @@ const SearchBar = () => {
   useEffect(()=>{
     console.log(location.pathname)
      if(location.pathname.includes('collection') && showSearch){
-       setVisible
+       setVisible(true)
      }
   },[location])
 
