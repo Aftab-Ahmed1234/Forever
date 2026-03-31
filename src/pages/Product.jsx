1443917c0ebc47da92c449e48a0d1a9be5cc04e1
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-03-31 18:45:21 +0500

    changing

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -11,6 +11,7 @@ const Product = () => {
        products.map((item)=>{
         if(item.id===productId){
           setProductData(item)
+          console.log(item)
           return null;
       }
   })
