fd87d17a148a3e10f4b4c9f9537ae1c0235849d0
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-04-12 18:33:59 +0500

    changing

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,16 +4,16 @@ import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext'
 
 const Product = () => {
-  const {productId}=useParams()
+  const {productId}=useParams();
   const {products}=useContext(ShopContext)
   const[productData, setProductData]=useState(false)
   const[image, setImage]=useState('')
   const fetchProductData = async () => {
        products.map((item)=>{
-        if(item.id===productId){
+        if(item._id===productId){
           setProductData(item)
           setImage(item.image[0])
-          console.log(item)
+    
           return null;
       }
   })
@@ -22,18 +22,30 @@ const Product = () => {
     fetchProductData()
   },[productId])
   return  productData ? (
-    <div className='border-t-2 pt-10 transition-opacity duration-500 opacity-100'>
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
         {/* Product Data */}
         <div className='flex gap-12 sm:gap-12  flex-col sm:flex-row'>
           {/* Product Image*/}
-          <div className='flex-1 fle flex-col-reverse gap-3 sm:flex-row'>
-            <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll w-full sm:justify-none sm:flex-col'>
-                  {
-                    productData.image.map((item,index)=>{
-                      <img src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' />
-                    })
-                  }
-            </div>
+          <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+            <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+  
+  {/* Thumbnails */}
+  <div className='flex flex-col gap-3 overflow-y-auto max-h-[500px]'>
+    {
+      productData.image.map((item, index) => (
+        <img
+          src={item}
+          key={index}
+          onClick={() => setImage(item)}
+          className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer '
+        />
+      ))
+    }
+  </div>
+
+
+
+</div>
           </div>
 
         </div>
