cf9822f21c0c2615dfbce058d78b2a1b4ba925a5
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-04-22 19:47:58 +0500

    changing

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -52,7 +52,7 @@ const Product = () => {
             <img src={assets.star_icon} alt="" className="w-3.5" />
             <img src={assets.star_icon} alt="" className="w-3.5" />
             <img src={assets.star_dull_icon} alt="" className="w-3.5" />
-            
+            <p></p>
           </div>
         </div>
       </div>
