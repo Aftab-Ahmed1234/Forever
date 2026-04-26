a3b857dca75dc9480d4f6652f70e36f5d665c103
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-04-26 12:22:38 +0500

    changing

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -55,6 +55,7 @@ const Product = () => {
             <p className='ol-2'>(122)</p>
           </div>
           <p className='mt-5 text-xl font-medium'>{currency}{productData.price}</p>
+          <p>{}</p>
         </div>
       </div>
     </div>
