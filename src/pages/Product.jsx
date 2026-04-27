0d49155e984900cff26702a5c28fa210270f0076
Aftab-Ahmed1234 <aftabbscs6@gmail.com>
2026-04-27 11:01:47 +0500

    changing

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -58,8 +58,8 @@ const Product = () => {
           <p className='mt-5 text-gray-500 md: w-4/5'>{productData.description}</p>
           <div className='flex flex-col gap-4 my-8'>
             <p>Select Size</p>
-            <div>
-              
+            <div className=''>
+
             </div>
           </div>
         </div>
